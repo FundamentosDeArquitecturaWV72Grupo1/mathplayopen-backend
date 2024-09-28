9232b3d2bb7933d1b54ab24406333d3b391fd785
Juan Diego <164110630+JDu202012207@users.noreply.github.com>
2024-09-28 05:26:34 -0500

    chore: initial commit.

---
+++ src/main/java/com/_squared/mathplayopen/MathplayopenApplication.java
@@ -0,0 +1,13 @@
+package com._squared.mathplayopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathplayopenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathplayopenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_squared/mathplayopen/MathplayopenApplicationTests.java
@@ -0,0 +1,13 @@
+package com._squared.mathplayopen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathplayopenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
