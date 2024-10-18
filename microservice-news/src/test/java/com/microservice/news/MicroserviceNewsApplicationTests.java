71c3828e807cc7aee3d2321a55dda82ba8755313
MiguelC <sackito992@gmail.com>
2024-10-18 14:21:29 -0500

    feat: add news microservice

---
+++ microservice-news/src/test/java/com/microservice/news/MicroserviceNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
