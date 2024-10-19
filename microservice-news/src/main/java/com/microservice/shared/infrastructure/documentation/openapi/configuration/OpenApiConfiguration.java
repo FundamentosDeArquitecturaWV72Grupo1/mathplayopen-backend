4c4c3c16d5b98b3b8ebbb296f7a8b7ee84fe5a08
MiguelC <sackito992@gmail.com>
2024-10-19 08:01:55 -0500

    feat: add dependencies

---
+++ microservice-news/src/main/java/com/microservice/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,45 @@
+package com.microservice.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * OpenAPI configuration class.
+ * This class is responsible for configuring the OpenAPI documentation for the application.
+
+ */
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("MathPlayOpen - UPC")
+                        .description("Review Service application REST API documentation.")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Review Service")
+                        .url("https://final-project-opensource.github.io/LandingPageFitHub/"));
+
+//        final String securitySchemeName = "bearerAuth";
+//        openApi.addSecurityItem(new SecurityRequirement()
+//                        .addList(securitySchemeName))
+//                .components(new Components()
+//                        .addSecuritySchemes(securitySchemeName,
+//                                new SecurityScheme()
+//                                        .name(securitySchemeName)
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")));
+        return openApi;
+    }
+}
---
+++ microservice-news/src/main/java/com/microservice/shared/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package com.microservice.shared.resources;
+
+public record MessageResource(String message) {
+}
